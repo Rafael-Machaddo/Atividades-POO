6a3ca148c411cca281e7d4ab8721584f2d3caf93
Rafael Machado <62083559+Rafael-Machaddo@users.noreply.github.com>
2021-06-25 18:25:10 -0300

    Add files via upload

---
+++ POO-Exercícios sobre herança/1-exercicio/teste/Main.java
@@ -0,0 +1,24 @@
+package teste;
+public class main {
+public static void main(string [] args);
+mouse mouse = new mouse ();
+mouse.metodo();
+optico optico = new optico ();
+wireless wireless = new wireless ();
+Bluetooth bluetooth = new Bluetooth();
+bluetooth.bluetooth();
+
+public class Mouse {
+public void optico () {
+System.out.println("Óptico");
+}
+
+public void wireless() {
+    System.out.println("wireless");
+
+}
+
+public void bluetooth() {
+	System.out.println("bluetooth");
+}
+}
---
+++ POO-Exercícios sobre herança/1-exercicio/testes/optico_extend_mouse.java
@@ -0,0 +1,21 @@
+package testes;
+
+public class optico_extend_mouse {
+
+public void optico () {
+	System.out.println("óptico");
+	
+}
+public class wireless_extend_mouse {
+	public void wireless () {
+		System.out.println("wireless");
+}
+}
+
+public class Bluetooth_extend_mouse {
+	public void bluetooth () {
+		System.out.println("Bluetooth");
+	}
+}
+	}
+}
